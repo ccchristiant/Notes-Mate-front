280cb8db4a045f2784444d5a4a1efb55f7fa7a64
Christian Tran <christian.tran@epitech.eu>
2022-09-02 21:29:42 +0200

    [ADD] Ep1 : 1h07min49s TS part with fade out truncation hiding/showing

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('truncator') truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText!: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    //work out if there is a text overflow and if not, hide the truncator
+
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      // else there is no text overflow, hide the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
   }
 
 }
