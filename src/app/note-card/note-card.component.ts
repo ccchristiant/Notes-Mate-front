3aefdb6297960ce935e0030527e27ffc4fc8e488
Christian Tran <christian.tran@epitech.eu>
2022-09-02 21:41:29 +0200

    [ADD] Dynamic text with Input : Ep1 done

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent implements AfterViewInit {
+export class NoteCardComponent implements OnInit, AfterViewInit {
+
+  @Input() title!: string;
+  @Input() body!: string;
 
   @ViewChild('truncator') truncator!: ElementRef<HTMLElement>;
   @ViewChild('bodyText') bodyText!: ElementRef<HTMLElement>;
 
   constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+  }
 
   ngAfterViewInit() {
     //work out if there is a text overflow and if not, hide the truncator
